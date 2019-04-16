b0fc527da4fdb2034dc7de57ad228cccb0350818
lashanfaliq95 <lashanfaliq@gmail.com>
2019-04-16 15:03:31 +0530

    add build

---
+++ build/precache-manifest.d30fa2eed0579eee99c4469dcecd5fd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "207a3d951eff5434b3e79d6af1e5738b",
+    "url": "/static/media/login_background.207a3d95.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "02305784f5b104be250f",
+    "url": "/static/js/main.5111920c.chunk.js"
+  },
+  {
+    "revision": "0d72dde127729c9c644a",
+    "url": "/static/js/2.f62e26bb.chunk.js"
+  },
+  {
+    "revision": "02305784f5b104be250f",
+    "url": "/static/css/main.81c91619.chunk.css"
+  },
+  {
+    "revision": "0d72dde127729c9c644a",
+    "url": "/static/css/2.4292272f.chunk.css"
+  },
+  {
+    "revision": "94432a672023fac8051255708015f066",
+    "url": "/index.html"
+  }
+];
